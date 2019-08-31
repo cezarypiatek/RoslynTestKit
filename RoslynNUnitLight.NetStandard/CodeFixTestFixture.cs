85895d9b68e64266de67ebe679813eeae2683469
cezarypiatek <cezarypiatek@live.com>
2019-08-31 12:33:57 +0200

    Refactor CodeFixTestFixture to use IDiagnosticLocator

--- RoslynNUnitLight.NetStandard/CodeFixTestFixture.cs
+++ RoslynNUnitLight.NetStandard/CodeFixTestFixture.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
@@ -13,51 +12,70 @@ namespace RoslynNUnitLight
 {
     public abstract class CodeFixTestFixture : BaseTestFixture
     {
+        protected virtual ImmutableList<MetadataReference> References => null;
         protected abstract CodeFixProvider CreateProvider();
 
         protected void TestCodeFix(string markupCode, string expected, string diagnosticId, int codeFixIndex = 0)
         {
-            var (document, span) = GetDocumentAndSpanFromMarkup(markupCode);
-            var reportedDiagnostics = GetReportedDiagnostics(document).ToList();
+            var document = MarkupHelper.GetDocumentFromMarkup(markupCode, LanguageName, References);
+            var locator = MarkupHelper.GetLocator(markupCode);
+            TestCodeFix(document, expected, diagnosticId, locator, codeFixIndex);
+        }
+
+        protected void TestCodeFixAtLine(string code, string expected, string diagnosticId, int line, int codeFixIndex = 0)
+        {
+            var document = MarkupHelper.GetDocumentFromMarkup(code, LanguageName, References);
+            var locator = LineLocator.FromCode(code, line);
+            TestCodeFix(document, expected, diagnosticId, locator, codeFixIndex);
+        }
+
+        protected void TestCodeFixAtLine(string code, string expected, DiagnosticDescriptor descriptor, int line, int codeFixIndex = 0)
+        {
+            var document = MarkupHelper.GetDocumentFromMarkup(code, LanguageName, References);
+            var locator = LineLocator.FromCode(code, line);
+            TestCodeFix(document, expected, descriptor, locator, codeFixIndex);
+        }
+        protected void TestCodeFix(string markupCode, string expected, DiagnosticDescriptor descriptor, int codeFixIndex = 0)
+        {
+            var document = MarkupHelper.GetDocumentFromMarkup(markupCode, LanguageName, References);
+            var locator = MarkupHelper.GetLocator(markupCode);
+            TestCodeFix(document, expected, descriptor, locator, codeFixIndex);
+        }
+
+        protected void TestCodeFix(Document document, string expected, DiagnosticDescriptor descriptor, TextSpan span, int codeFixIndex = 0)
+        {
+           var locator = new TextSpanLocator(span);
+           TestCodeFix(document, expected, descriptor, locator, codeFixIndex);
+        }
+
+        private void TestCodeFix(Document document, string expected, string diagnosticId, IDiagnosticLocator locator, int codeFixIndex)
+        {
+            var reportedDiagnostics = GetReportedDiagnostics(document, locator).ToList();
             var diagnostic = reportedDiagnostics.FirstOrDefault(x => x.Id == diagnosticId);
-            Assert.That(diagnostic, Is.Not.Null, ()=>
+            Assert.That(diagnostic, Is.Not.Null, () =>
             {
                 var reportedIssues = reportedDiagnostics.Select(x => x.Id).ToList();
                 return $"There is no issue reported for {diagnosticId}. Reported issues: {string.Join(",", reportedIssues)}";
             });
-            TestCodeFix(document, span, expected, diagnostic.Descriptor, codeFixIndex);
+            TestCodeFix(document, expected, diagnostic.Descriptor, locator, codeFixIndex);
         }
 
-        protected void TestCodeFix(string markupCode, string expected, DiagnosticDescriptor descriptor, int codeFixIndex = 0)
+        private void TestCodeFix(Document document, string expected, DiagnosticDescriptor descriptor, IDiagnosticLocator locator, int codeFixIndex = 0)
         {
-            var (document, span) = GetDocumentAndSpanFromMarkup(markupCode);
-            TestCodeFix(document, span, expected, descriptor, codeFixIndex);
-        }
-
-        protected void TestCodeFix(Document document, TextSpan span, string expected, DiagnosticDescriptor descriptor, int codeFixIndex = 0)
-        {
-            var codeFixes = GetCodeFixes(document, span, descriptor);
+            var codeFixes = GetCodeFixes(document, locator, descriptor);
             Assert.That(codeFixes.Length, Is.AtLeast(codeFixIndex + 1));
             Verify.CodeAction(codeFixes[codeFixIndex], document, expected);
         }
 
-        private (Document document, TextSpan span) GetDocumentAndSpanFromMarkup(string markupCode)
+        private static IEnumerable<Diagnostic> GetReportedDiagnostics(Document document, IDiagnosticLocator locator)
         {
-            Assert.That(TestHelpers.TryGetDocumentAndSpanFromMarkup(markupCode, LanguageName, out var document, out var span), Is.True, "Cannot find any text span marked with '[||]'");
-            return (document, span);
+            return document.GetSemanticModelAsync().Result.GetDiagnostics().Where(d=> locator.Match(d.Location));
         }
 
-
-        private static IEnumerable<Diagnostic> GetReportedDiagnostics(Document document)
-        {
-            return document.GetSemanticModelAsync().Result.GetDiagnostics();
-        }
-
-
-        private ImmutableArray<CodeAction> GetCodeFixes(Document document, TextSpan span, DiagnosticDescriptor descriptor)
+        private ImmutableArray<CodeAction> GetCodeFixes(Document document, IDiagnosticLocator locator, DiagnosticDescriptor descriptor)
         {
             var builder = ImmutableArray.CreateBuilder<CodeAction>();
-            var diagnostic = GetDiagnosticForDescriptor(document, span, descriptor);
+            var diagnostic = FindOrCreateDiagnosticForDescriptor(document, descriptor, locator);
             var context = new CodeFixContext(document, diagnostic, (a, _) => builder.Add(a), CancellationToken.None);
 
             var provider = CreateProvider();
@@ -66,9 +84,9 @@ namespace RoslynNUnitLight
             return builder.ToImmutable();
         }
 
-        private static Diagnostic GetDiagnosticForDescriptor(Document document, TextSpan span, DiagnosticDescriptor descriptor)
+        private static Diagnostic FindOrCreateDiagnosticForDescriptor(Document document, DiagnosticDescriptor descriptor, IDiagnosticLocator locator)
         {
-            var reportedDiagnostics = GetReportedDiagnostics(document).ToList();
+            var reportedDiagnostics = GetReportedDiagnostics(document, locator).ToList();
             var diagnostic = reportedDiagnostics.FirstOrDefault(x => x.Id == descriptor.Id);
             if (diagnostic != null)
             {
@@ -76,7 +94,7 @@ namespace RoslynNUnitLight
             }
 
             var tree = document.GetSyntaxTreeAsync(CancellationToken.None).Result;
-            return Diagnostic.Create(descriptor, Location.Create(tree, span));
+            return Diagnostic.Create(descriptor, Location.Create(tree, locator.GetSpan()));
         }
     }
 }
--- RoslynNUnitLight.NetStandard/Utils/LineLocator.cs
+++ RoslynNUnitLight.NetStandard/Utils/LineLocator.cs
@@ -1,14 +1,20 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
 
 namespace RoslynNUnitLight
 {
     class LineLocator : IDiagnosticLocator
     {
         private readonly int line;
+        private readonly int _startPosition;
+        private readonly int _endPosition;
 
-        public LineLocator(int line)
+        public LineLocator(int line, int startPosition, int endPosition)
         {
             this.line = line;
+            _startPosition = startPosition;
+            _endPosition = endPosition;
         }
 
         public bool Match(Location location)
@@ -19,6 +25,24 @@ namespace RoslynNUnitLight
                 location.IsInSource &&
                 lineSpan.StartLinePosition.Line <= zeroBaseLine &&
                 lineSpan.EndLinePosition.Line >= zeroBaseLine;
+        }
+
+        public TextSpan GetSpan()
+        {
+            return new TextSpan(_startPosition, _endPosition);
+        }
+
+        public static LineLocator FromCode(string code, int lineNumber)
+        {
+            var lineStart = code.IndexOf("\n", 0, lineNumber,StringComparison.InvariantCulture);
+            var lineEnd = code.IndexOf("\n", 0, lineNumber+1,StringComparison.InvariantCulture);
+            return new LineLocator(lineNumber, lineStart, lineEnd);
+        }
+
+        public static LineLocator FromDocument(Document document, int lineNumber)
+        {
+            var sourceCode = document.GetSyntaxRootAsync().GetAwaiter().GetResult().ToFullString();
+            return FromCode(sourceCode, lineNumber);
         }
     }
 }
