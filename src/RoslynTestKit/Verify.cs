5207edefd592f400265902a1487d6e76148db048
cezarypiatek <cezarypiatek@live.com>
2019-08-31 16:23:36 +0200

    Present code difference using diff tool

--- src/RoslynTestKit/Verify.cs
+++ src/RoslynTestKit/Verify.cs
@@ -1,5 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Threading;
+using ApprovalTests.Reporters;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 
@@ -30,7 +34,22 @@ namespace RoslynTestKit
             var text = sourceText.ToString();
             if (text != expectedCode)
             {
+                TryToReportDiff(expectedCode, text);
                 throw new  TransformedCodeDifferentThanExpectedException(text, expectedCode);
+            }
+        }
+
+        private static void TryToReportDiff(string expectedCode, string text)
+        {
+            if (Debugger.IsAttached)
+            {
+                var tmpDir = Path.GetTempPath();
+                var tempFileName = Guid.NewGuid().ToString("N").Substring(0, 4);
+                var transformedPath = Path.Combine(tmpDir, $"{tempFileName}_transformed.cs");
+                File.WriteAllText(transformedPath, text);
+                var expectedPath = Path.Combine(tmpDir, $"{tempFileName}_expected.cs");
+                File.WriteAllText(expectedPath, expectedCode);
+                new DiffReporter().Report( transformedPath, expectedPath);
             }
         }
     }
