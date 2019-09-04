f142a802dd9e684313700346139ccb7a3049c67e
cezarypiatek <cezarypiatek@live.com>
2019-09-04 21:28:18 +0200

    Create CompletionProviderFixture for testing completion providers

--- src/RoslynTestKit/RoslynTestKitException.cs
+++ src/RoslynTestKit/RoslynTestKitException.cs
@@ -4,6 +4,7 @@ using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.Completion;
 using RoslynTestKit.Utils;
 
 namespace RoslynTestKit
@@ -21,31 +22,30 @@ namespace RoslynTestKit
 
         public static RoslynTestKitException DiagnosticNotFound(string diagnosticId, IDiagnosticLocator locator, Diagnostic[] reportedDiagnostics)
         {
-            var reportedIssues = reportedDiagnostics.Select(x => x.Id).ToList();
-            var message = $"There is no issue reported for {diagnosticId} at {locator}. Reported issues: {string.Join(",", reportedIssues)}";
+            var message = $"There is no issue reported for {diagnosticId} at {locator}. Reported issues: {reportedDiagnostics.MergeWithComma(x=>x.Id)}";
             return new RoslynTestKitException(message);
         }
 
         public static RoslynTestKitException CodeFixNotFound(int expectedCodeFixIndex, ImmutableArray<CodeAction> codeFixes)
         {
-            var message = $"Cannot find CodeFix with index {expectedCodeFixIndex}. Found only {codeFixes.Length} CodeFixes: {string.Join(",", codeFixes.Select((x, index) => $"[{index}] = {x.Title}"))}";
+            var message = $"Cannot find CodeFix with index {expectedCodeFixIndex}. Found only {codeFixes.Length} CodeFixes: {GetActionsDescription(codeFixes)}";
             return new RoslynTestKitException(message);
         }
 
         public static RoslynTestKitException CodeRefactoringNotFound(int expectedCodeRefactoringIndex, ImmutableArray<CodeAction> codeRefactorings)
         {
-            var refactoringDescriptions = GetRefactoringDescriptions(codeRefactorings);
+            var refactoringDescriptions = GetActionsDescription(codeRefactorings);
             var message = $"Cannot find CodeRefactoring with index {expectedCodeRefactoringIndex}. Found only {codeRefactorings.Length} CodeRefactorings: {refactoringDescriptions}";
             return new RoslynTestKitException(message);
         }
 
         public static RoslynTestKitException UnexpectedCodeRefactorings(ImmutableArray<CodeAction> codeRefactorings)
         {
-            var refactoringDescriptions = GetRefactoringDescriptions(codeRefactorings);
+            var refactoringDescriptions = GetActionsDescription(codeRefactorings);
             return new RoslynTestKitException($"Found reported CodeRefactorings '{refactoringDescriptions}' in spite of the expectations ");
         }
 
-        private static string GetRefactoringDescriptions(ImmutableArray<CodeAction> codeFixes)
+        private static string GetActionsDescription(ImmutableArray<CodeAction> codeFixes)
         {
             return string.Join(", ", codeFixes.Select((x, index) => $"[{index}] = {x.Title}"));
         }
@@ -57,8 +57,12 @@ namespace RoslynTestKit
 
         public static RoslynTestKitException MoreThanOneOperationForCodeAction(CodeAction codeAction, List<CodeActionOperation> operations)
         {
-            var operationDescriptions = string.Join(", ", operations.Select(x => x.Title));
-            return new RoslynTestKitException($"There is more than one operation associated with '{codeAction.Title}'. Found operations: {operationDescriptions}");
+            return new RoslynTestKitException($"There is more than one operation associated with '{codeAction.Title}'. Found operations: {operations.MergeWithComma(x=>x.Title)}");
+        }
+
+        public static Exception CannotFindSuggestion(IReadOnlyList<string> missingCompletion, ImmutableArray<CompletionItem> resultItems)
+        {
+            return new RoslynTestKitException($"Cannot get suggestions:\r\n{missingCompletion.MergeAsBulletList()}\r\nFound suggestions:\r\n{resultItems.MergeAsBulletList(x=>x.DisplayText)}");
         }
     }
 }
---
+++ src/RoslynTestKit/Utils/StringHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RoslynTestKit.Utils
+{
+    internal static class StringHelpers
+    {
+        public static string MergeWithComma<T>(this IReadOnlyList<T> list, Func<T, string> map=null)
+        {
+            return string.Join(", ", list.Select(map ?? (x => x.ToString())));
+        }
+
+        public static string MergeAsBulletList<T>(this IReadOnlyList<T> list, Func<T, string> map=null)
+        {
+            var mapItem = map ?? (x => x.ToString());
+            return string.Join(Environment.NewLine, list.Select(x => $"- {mapItem(x)}"));
+        }
+    }
+}
