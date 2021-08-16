9fc27b79ddd4b3ab401d4fc9b3e634f4cad3fd8c
Cezary Piątek <cezarypiatek@live.com>
2021-08-16 22:40:45 +0200

    Add option to select code action by a title

---
+++ src/RoslynTestKit/CodeActionLocators/ICodeActionSelector.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis.CodeActions;
+
+namespace RoslynTestKit.CodeActionLocators
+{
+    public interface ICodeActionSelector
+    {
+        public CodeAction Find(IReadOnlyList<CodeAction> actions);
+    }
+}
