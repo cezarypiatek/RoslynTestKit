2615799920dd2149efddcce17bda4f36d7d409bb
Cezary Piatek <cezarypiatek@live.com>
2024-01-03 18:36:50 +0100

    Fix method visibility

--- src/RoslynTestKit/RoslynFixtureFactory.cs
+++ src/RoslynTestKit/RoslynFixtureFactory.cs
@@ -7,56 +7,46 @@ namespace RoslynTestKit
 {
     public static class RoslynFixtureFactory
     {
-        public static AnalyzerTestFixture Create<T>(AnalyzerTestFixtureConfig? config = null) 
-            where T : DiagnosticAnalyzer, new()
+        public static AnalyzerTestFixture Create<T>(AnalyzerTestFixtureConfig? config = null) where T : DiagnosticAnalyzer, new()
         {
             var diagnosticAnalyzer = new T();
             return Create(diagnosticAnalyzer, config);
         }
 
-        public static AnalyzerTestFixture Create<T>(T diagnosticAnalyzer, AnalyzerTestFixtureConfig? config = null) 
-            where T : DiagnosticAnalyzer
+        public static AnalyzerTestFixture Create(DiagnosticAnalyzer diagnosticAnalyzer, AnalyzerTestFixtureConfig? config = null) 
         {
-         
             return new ConfigurableAnalyzerTestFixture(diagnosticAnalyzer, config ?? new AnalyzerTestFixtureConfig());
         }
         
-        public static CodeFixTestFixture Create<T>(CodeFixTestFixtureConfig? config = null) 
-            where T : CodeFixProvider, new()
+        public static CodeFixTestFixture Create<T>(CodeFixTestFixtureConfig? config = null) where T : CodeFixProvider, new()
         {
             var codeFixProvider = new T();
             return Create(codeFixProvider, config);
         }
 
-        private static CodeFixTestFixture Create<T>(T codeFixProvider, CodeFixTestFixtureConfig? config = null) 
-            where T : CodeFixProvider
+        public static CodeFixTestFixture Create(CodeFixProvider codeFixProvider, CodeFixTestFixtureConfig? config = null) 
         {
             return new ConfigurableCodeFixTestFixture(codeFixProvider, config ?? new CodeFixTestFixtureConfig());
         }
         
-        public static CodeRefactoringTestFixture Create<T>(CodeRefactoringTestFixtureConfig? config = null) 
-            where T : CodeRefactoringProvider, new()
+        public static CodeRefactoringTestFixture Create<T>(CodeRefactoringTestFixtureConfig? config = null) where T : CodeRefactoringProvider, new()
         {
             var codeRefactoringProvider = new T();
             return Create(codeRefactoringProvider, config);
         }
 
-        private static CodeRefactoringTestFixture Create<T>(T codeRefactoringProvider, CodeRefactoringTestFixtureConfig? config = null) 
-            where T : CodeRefactoringProvider
+        public static CodeRefactoringTestFixture Create(CodeRefactoringProvider codeRefactoringProvider, CodeRefactoringTestFixtureConfig? config = null) 
         {
             return new ConfigurableCodeRefactoringTestFixture(codeRefactoringProvider, config ?? new CodeRefactoringTestFixtureConfig());
         }
         
-        
-        public static CompletionProviderFixture Create<T>(CompletionProviderTestFixtureConfig? config = null) 
-            where T : CompletionProvider, new()
+        public static CompletionProviderFixture Create<T>(CompletionProviderTestFixtureConfig? config = null) where T : CompletionProvider, new()
         {
             var codeRefactoringProvider = new T();
             return Create(codeRefactoringProvider, config);
         }
 
-        private static ConfigurableCompletionProviderTestFixture Create<T>(T completionProvider, CompletionProviderTestFixtureConfig? config = null) 
-            where T : CompletionProvider
+        private static ConfigurableCompletionProviderTestFixture Create(CompletionProvider completionProvider, CompletionProviderTestFixtureConfig? config = null) 
         {
             return new ConfigurableCompletionProviderTestFixture(completionProvider, config ??  new CompletionProviderTestFixtureConfig());
         }
